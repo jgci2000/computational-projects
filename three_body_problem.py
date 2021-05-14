764d3a9449724e13059d5202f63ffe965e17ae22
João Inácio <jgci2000@hotmail.com>
2021-05-14 16:21:56 +0100

    removed yt script and added three_body_problem

---
+++ three_body_problem.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import vpython as vp
+
+
+# Simulation of 2 planets orbition around the sun
+
+# Masses on kg
+M_sun = 2e^30
+print(M_sun)
+
+masses = np.array([])
+
+
+
+
+
+
+
+
+
+
+
+
